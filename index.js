c4910299d9352a56b187d6caef1fcbd54f715897
AlahiraJ <alahirajeffrey@gmail.com>
2021-06-06 23:17:09 +0100

    added middleware for parsing request data

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const userInformation = require("./database/userInfo")
 const express = require("express")
+const database = require("./database/userDatabase")
+const bodyParser = require("body-parser")
+const userRoute = require('./routes/userRoutes')
 
 const app = express()
 
+// parse request data
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+//setup user routes
+app.use(userRoute)
+
+//set port number
+const port = process.env.PORT || 2555
+
+app.listen(port, ()=>{
+    console.log(`Listening on port: ${port}`)
+})
