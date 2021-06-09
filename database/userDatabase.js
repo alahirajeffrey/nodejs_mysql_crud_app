ae5064ecd1084a787e9733998f1f75e964b3de47
AlahiraJ <alahirajeffrey@gmail.com>
2021-06-09 10:10:43 +0100

    refactored database code

--- database/userDatabase.js
+++ database/userDatabase.js
@@ -7,31 +7,32 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: env.USER ||'nodeuser',
     password: env.PASSWORD || 'nodeuserpassword',
-    port: env.PORT || 3306
+    port: env.PORT || 3306,
+    database: 'userInformation'
 })
 
 //connect to database
 connection.connect((err)=>{
     if (err) throw err
-    console.log('Connected to sql service...')
+    console.log('Connected to database...')
 
     //create database
-    connection.query("CREATE DATABASE IF NOT EXISTS userInformation", (err, result)=>{
-        if (err) throw err
-        console.log("Database created...")
+    // connection.query("CREATE DATABASE IF NOT EXISTS userInformation", (err, result)=>{
+    //     if (err) throw err
+    //     console.log("Database created...")
 
-        //create table
-        const table = `CREATE TABLE IF NOT EXISTS userInformation.user (
-            name VARCHAR (255),
-            nationality VARCHAR (255),
-            email VARCHAR (255), 
-            mobile INT (15)
-        )`
-        connection.query(table, (err, result)=>{
-            if (err) throw err
-            console.log('table created...')
-        })
-    })
+    //     //create table
+    //     const table = `CREATE TABLE IF NOT EXISTS userInformation.user (
+    //         name VARCHAR (255),
+    //         nationality VARCHAR (255),
+    //         email VARCHAR (255), 
+    //         mobile INT (15)
+    //     )`
+    //     connection.query(table, (err, result)=>{
+    //         if (err) throw err
+    //         console.log('table created...')
+    //     })
+    // })
 })
 
 module.exports = connection
