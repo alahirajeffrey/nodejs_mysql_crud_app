7c7210da290efac18bffdc4f24ae3f1d094e533f
AlahiraJ <alahirajeffrey@gmail.com>
2021-06-09 10:14:36 +0100

    completed user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,23 +14,54 @@ router.post('/createUser', (req, res)=>{
         return res.status(400).send('Oops.. Request body missing')
     }
     
-    //save request to variables
-    const name = req.body.name
-    const nationality = req.body.nationality
-    const email = req.body.email
-    const mobile = req.body.mobile
-    
     //save request to database
-    const user = {name: name, nationality: nationality, email: email, mobile: mobile}
-    connection.query('INSERT INTO userInformation.user SET ?', user, (err, result)=>{
+    const user = {name: req.body.name, nationality: req.body.nationality, email: req.body.email, mobile: req.body.mobile}
+    connection.query('INSERT INTO user SET ?', user, (err, result)=>{
         if (err) throw err
         console.log('User record saved')
         res.send('User saved')
     })
 })
 
-//find one user
 //find all users
+router.get("/findAll", (req, res)=>{
+    //query database
+    connection.query('SELECT * FROM user', (err, result)=>{
+        if (err) throw err
+        res.send(result)
+        console.log("Users found")
+    })
+})
+
+//find one users
+router.get("/findOne", (req, res)=>{
+    //validate request
+    if(!req.body.name){
+        return res.status(400).status("Oops. Request body missing")
+    }
+
+    //query database
+    connection.query('SELECT * FROM user WHERE name = ?', [req.body.name], (err, result)=>{
+        if (err) throw err
+        res.send(result)
+        console.log("User found")
+    })
+
+})
+
 //delete user
+router.delete("/deleteUser", (req, res)=>{
+    //validate request
+    if(!req.body.name){
+        return res.status(400).status("Oops. request body missing")}
+
+    //query database
+    connection.query('DELETE FROM user WHERE name = ?', [req.body.name], (err, results)=>{
+        if (err) throw err
+        res.send("User deleted")
+        console.log("User deleted")
+          })
+    
+})
 
 module.exports = router
